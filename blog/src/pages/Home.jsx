c77a61f03f541ead8c293043cfda9f76cb1ae01a
baedollee <baesee0401@gmail.com>
2022-07-28 16:30:59 +0900

    bug fix

--- blog/src/pages/Home.jsx
+++ blog/src/pages/Home.jsx
@@ -7,12 +7,11 @@ import Done from '../components/Done/Done';
 
 const Home = () => {
   const [todoList, setTodoList] = useState([
-    { title: '타이틀2', contents: '컨텐츠2', done: false, id: 1 },
-    { title: '타이틀2', contents: '컨텐츠2', done: true, id: 2 },
+    { title: '타이틀', contents: '컨텐츠', done: false, id: 1 },
+    { title: '타이틀', contents: '컨텐츠', done: true, id: 2 },
   ]);
 
-  // 저기 리액트.usestate 왜 저렇게?
-  const [inputForm, setInputForm] = React.useState({
+  const [inputForm, setInputForm] = useState({
     title: '',
     contents: '',
     done: false,
@@ -83,9 +82,10 @@ const Wrap = styled.div`
 const InputTitle = styled.h2`
   display: block;
   font-size: 1.5em;
+
   margin-block-start: 0.83em;
   margin-block-end: 0.83em;
-  margin-inline-start: 0px;
+  margin-inline-start: 20px;
   margin-inline-end: 0px;
   font-weight: bold;
 `;
