ea8473dc26a9550ab73f8989c063e716a0cdc8cc
baedollee <baesee0401@gmail.com>
2022-08-03 10:59:49 +0900

    study

--- blog/src/components/Detail/Detail.jsx
+++ blog/src/components/Detail/Detail.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 const Detail = () => {
   const [item, setItem] = useState({});
   const params = useParams();
@@ -27,11 +30,72 @@ const Detail = () => {
   }
 
   return (
-    <div>
-      <p>{item.id}</p>
-      <p>{item.title}</p>
-      <p>{item.contents}</p>
-    </div>
+    <WrapDiv>
+      <BoxDiv>
+        <div>
+          <HeadDiv>
+            <p>ID:{item.id}</p>
+            <Btn>
+              <Link to={`../`}>이전으로</Link>
+            </Btn>
+          </HeadDiv>
+          <TitleH1>{item.title}</TitleH1>
+          <ContentH3>{item.contents}</ContentH3>
+        </div>
+      </BoxDiv>
+    </WrapDiv>
   );
 };
+
+const WrapDiv = styled.div`
+  border: 2px solid rgb(238, 238, 238);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+`;
+const BoxDiv = styled.div`
+  width: 600px;
+  height: 400px;
+  border: 1px solid rgb(238, 238, 238);
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+`;
+const HeadDiv = styled.div`
+  display: flex;
+  height: 80px;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  padding: 0px 24px;
+  -webkit-box-align: center;
+  align-items: center;
+`;
+const Btn = styled.button`
+  border: 1px solid rgb(221, 221, 221);
+  height: 40px;
+  width: 120px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 12px;
+  cursor: pointer;
+`;
+const TitleH1 = styled.h1`
+  padding: 0px 24px;
+  display: block;
+  font-size: 2em;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+`;
+const ContentH3 = styled.h3`
+  padding: 0px 24px;
+  display: block;
+`;
+
 export default Detail;
--- blog/src/components/Todo/Todo.jsx
+++ blog/src/components/Todo/Todo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { changeDoneList, deleteList } from '../../redux/reducer/listReducer';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Todo = ({ title, contents, id }) => {
   const dispatch = useDispatch();
